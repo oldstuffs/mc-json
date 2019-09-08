6f75c81b0d6950f00b99122068769f87f3ce4926
portlek <hsndmrts_@outlook.com>
2019-09-08 15:56:35 +0300

    pom update

--- mc-json/src/main/java/io/github/portlek/mcjson/JsonPlayerOf.java
+++ mc-json/src/main/java/io/github/portlek/mcjson/JsonPlayerOf.java
@@ -3,6 +3,17 @@ package io.github.portlek.mcjson;
 import io.github.portlek.mcjson.api.IJson;
 import io.github.portlek.mcjson.api.JsonPlayer;
 import io.github.portlek.mcjson.api.MckJson;
+import io.github.portlek.mcjson.nms.v1_10_R1.Json1_10_R1;
+import io.github.portlek.mcjson.nms.v1_11_R1.Json1_11_R1;
+import io.github.portlek.mcjson.nms.v1_12_R1.Json1_12_R1;
+import io.github.portlek.mcjson.nms.v1_13_R1.Json1_13_R1;
+import io.github.portlek.mcjson.nms.v1_13_R2.Json1_13_R2;
+import io.github.portlek.mcjson.nms.v1_14_R1.Json1_14_R1;
+import io.github.portlek.mcjson.nms.v1_8_R1.Json1_8_R1;
+import io.github.portlek.mcjson.nms.v1_8_R2.Json1_8_R2;
+import io.github.portlek.mcjson.nms.v1_8_R3.Json1_8_R3;
+import io.github.portlek.mcjson.nms.v1_9_R1.Json1_9_R1;
+import io.github.portlek.mcjson.nms.v1_9_R2.Json1_9_R2;
 import io.github.portlek.versionmatched.VersionMatched;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
@@ -10,7 +21,18 @@ import org.jetbrains.annotations.NotNull;
 public class JsonPlayerOf implements JsonPlayer {
 
     private static final IJson JSON = new VersionMatched<>(
-        new MckJson()
+        new MckJson(),
+        Json1_8_R1.class,
+        Json1_8_R2.class,
+        Json1_8_R3.class,
+        Json1_9_R1.class,
+        Json1_9_R2.class,
+        Json1_10_R1.class,
+        Json1_11_R1.class,
+        Json1_12_R1.class,
+        Json1_13_R1.class,
+        Json1_13_R2.class,
+        Json1_14_R1.class
     ).of().instance();
 
     @NotNull
